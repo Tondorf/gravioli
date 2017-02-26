67663618ca7dea66aeed10b18ee90af3647e3eef
Nis Meinert <mail@nismeinert.de>
2017-02-26 13:14:59 +0100

    reorder issues

--- server/logger.cpp
+++ server/logger.cpp
@@ -5,7 +5,7 @@
 #include "logger.h"
 
 namespace Log {
-    SimpleLogger::SimpleLogger() : _logLevel(LogLevel::INFO), _out(stdout) {
+    SimpleLogger::SimpleLogger() : _out(stdout), _logLevel(LogLevel::INFO) {
     }
 
     SimpleLogger::~SimpleLogger() {
