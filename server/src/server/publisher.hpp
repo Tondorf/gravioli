9d16c2ced5de01198ee46ac885b085219dae76bf
Nis Meinert <nis.meinert@gmail.com>
2017-08-20 17:20:01 +0200

    Changed FBS memory allocation, now using zero-copy
    Allocation of flatbuffers is now down dynamically on the heap. Doing so
    allows to zero-copy the messages to zeromq. Furthermore, I provided some
    comments in the code that (hopefully) explains this decision.

--- server/src/server/publisher.hpp
+++ server/src/server/publisher.hpp
@@ -1,22 +1,68 @@
 #pragma once
 
+#include <atomic>
+#include <chrono>
+#include <thread>
+
+#include "SimpleLogger/logger.hpp"
+
 #include "planet_generated.h"
 
 
 namespace server {
     class Publisher {
     public:
-        using BinaryData = flatbuffers::DetachedBuffer;
+        static constexpr std::size_t
+        SIMULTANEOUSLY_ALLOCATED_INSTANCES_THRESHOLD = 10;
 
-        std::vector<BinaryData> pop() {
-            flatbuffers::FlatBufferBuilder builder;
-            auto game = game::CreatePlanet(builder, 1.f, 2.f);
-            builder.Finish(game);
+        using BinaryData = flatbuffers::FlatBufferBuilder;
 
-            std::vector<BinaryData> data;
-            data.push_back(builder.Release());
+        static std::atomic<std::size_t> currentlyAllocatedInstances;
+
+        virtual ~Publisher() {
+            using namespace std::chrono_literals;
+
+            while (currentlyAllocatedInstances.load() > 0) {
+                Log::info("Waiting until all allocated instances of Publisher \
+                           are deleted...");
+
+                std::this_thread::sleep_for(1s);
+            }
+
+            Log::info("All allocated instances of Publisher are deleted.");
+        }
+
+
+        std::vector<BinaryData *> pop() {
+            if (currentlyAllocatedInstances.load() 
+                > SIMULTANEOUSLY_ALLOCATED_INSTANCES_THRESHOLD) {
+                Log::error("Number of simultaneously allocated instances\
+                            is above threshold: %d instances",
+                            currentlyAllocatedInstances.load());
+            }
+
+            using flatbuffers::FlatBufferBuilder;
+            FlatBufferBuilder *builder = new FlatBufferBuilder();
+
+            ++currentlyAllocatedInstances;
+
+            auto game = game::CreatePlanet(*builder, 1.f, 2.f);
+            builder->Finish(game);
+
+            std::vector<BinaryData *> data;
+            data.push_back(builder);
 
             return data;
         }
     };
+
+    std::atomic<std::size_t> Publisher::currentlyAllocatedInstances(0);
+
+
+    void customFree(void * /*data*/, void *hint) {
+        --Publisher::currentlyAllocatedInstances;
+
+        delete static_cast<flatbuffers::FlatBufferBuilder *>(hint);
+    }
 }
+
--- server/src/server/server.hpp
+++ server/src/server/server.hpp
@@ -13,6 +13,37 @@
 
 
 namespace server {
+    
+    extern void customFree(void * /*data*/, void * /*hint*/);
+
+    /*
+     * T has to provide the implementations of the member function:
+     *
+     * std::vector<S> pop()
+     *
+     * whereas S has to provide the implementations of the member functions:
+     *
+     * S'  GetBufferPointer()
+     * S'' GetSize()
+     *
+     * where the return values S' and S'' are convertible to byte * and
+     * std::size_t, respectively.
+     *
+     * After poping instances of S, Server will sent these encrypted over the
+     * wire and 
+     *
+     * (!) DELETE THEM AFTERWARDS (!)
+     *
+     * by calling 
+     *
+     * void customFree(void *s', void *t)
+     *
+     * s' and t are void pointers to respective S' and T instance, that was
+     * poped before.
+     * Not implementing this deallocation will lead dangling pointers!
+     * It is reasonable to implement this custom deallocation near by the
+     * implementation of T.
+     */
     template<class T>
     class Server {
     private:
@@ -27,34 +58,59 @@ namespace server {
         T _dataQueue;
 
 
-        bool sendBytes(byte *bytes, const std::size_t size, bool more = false) {
-            zmq_msg_t msg;
-
-            if (zmq_msg_init_size(&msg, size) != 0) {
+        bool sendMessage(zmq_msg_t *msg, const std::size_t size, bool more) {
+            int rc = zmq_msg_send(msg, _publisher, more ? ZMQ_SNDMORE : 0);
+            if (static_cast<std::size_t>(rc) != size) {
                 return false;
-            }
-
-            memcpy(zmq_msg_data(&msg), bytes, size);
-
-            {
-                int rc = zmq_msg_send(&msg, _publisher, more ? ZMQ_SNDMORE : 0);
-                if (static_cast<std::size_t>(rc) != size) {
-                    return false;
-                }
             }
 
             return true;
         }
 
+        bool sendBytes(byte *bytes, const std::size_t size, bool more) {
+            zmq_msg_t msg;
+
+            int rc = zmq_msg_init_size(&msg, size);
+            if (rc != 0) {
+                return false;
+            }
+
+            memcpy(zmq_msg_data(&msg), bytes, size);
+
+            return sendMessage(&msg, size, more);
+        }
+
+        bool sendBytes(byte *bytes,
+                       void *memOwner,
+                       const std::size_t size,
+                       bool more) {
+            zmq_msg_t msg;
+            
+            int rc = zmq_msg_init_data(&msg, bytes, size, customFree, memOwner);
+            if (rc != 0) {
+                return false;
+            }
+
+            return sendMessage(&msg, size, more);
+        }
+
 
         bool cryptAndSendBytes(byte *bytes, 
+                               void *memOwner,
                                const std::size_t size,
                                const crypto::Key& key,
                                bool more = false) {
             auto iv = crypto::encrypt(bytes, size, key);
 
+            /*
+             * zeromq allocates memory on the heap, when 'size' is above some
+             * threshold. In this case it is faster to have the data dynamically
+             * allocated, too and save the memcpy step.
+             * Typically, IV is below threshold, therefore the technique using
+             * memcpy is faster / non-critically.
+             */
             return sendBytes(&iv[0], crypto::IV_BLOCKSIZE, true) &&
-                   sendBytes(bytes, size, more);
+                   sendBytes(bytes, memOwner, size, more);
         }
 
 
@@ -71,7 +127,8 @@ namespace server {
             _context = zmq_ctx_new();
             _publisher = zmq_socket(_context, ZMQ_PUB);
 
-            std::string endpoint = std::string("tcp://*:") + std::to_string(_port);
+            std::string endpoint = std::string("tcp://*:")
+                                 + std::to_string(_port);
             if (zmq_bind(_publisher, endpoint.c_str()) != 0) {
                 Log::error("Could not bind to %s", endpoint.c_str());    
             } else { // successful binding
@@ -102,11 +159,16 @@ namespace server {
             };
 
             while (!_stopped) {
-                for (auto&& data : _dataQueue.pop()) {
-                    auto bytes = static_cast<byte *>(data.data());
-                    auto size = static_cast<std::size_t>(data.size());
+                for (auto container : _dataQueue.pop()) {
 
-                    if (!cryptAndSendBytes(bytes, size, KEY)) {
+                    auto bytes = static_cast<byte *>(
+                        container->GetBufferPointer()
+                    );
+                    auto size = static_cast<std::size_t>(
+                        container->GetSize()
+                    );
+
+                    if (!cryptAndSendBytes(bytes, container, size, KEY)) {
                         Log::error("Error during message transmission");
                     }
                 }
