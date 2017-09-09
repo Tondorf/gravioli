fb0baa0753cc896bf5b1ffaa9a0adbd8e430876a
Nis Meinert <mail@nismeinert.de>
2017-09-09 15:09:46 +0200

    Added whitespace after log level

--- server/test/main.cxx
+++ server/test/main.cxx
@@ -5,6 +5,9 @@
 
 int main(int argc, char **argv) {
     Log::SimpleLogger::getInstance().setLogLevel(Log::LogLevel::ERROR);
+    Log::SimpleLogger::getInstance().addTimestamp([]() {
+        return "";
+    });
 
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
