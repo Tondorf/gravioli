afab619d0854fd20502e2a22f8c68bfbc9e45838
Nis Meinert <mail@nismeinert.de>
2017-10-22 14:06:07 +0200

    Server sleeps for time after MsgQueue is empty.
    This time is given in config.hpp.in

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -107,8 +107,7 @@ namespace server {
 
 
     void Server::sleep_outer() {
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(1s);
+        std::this_thread::sleep_for(SERVER_SLEEP);
     }
 
 
