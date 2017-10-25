38bd14717d585f54fda2bc579bcf16150b3349d5
Nis Meinert <mail@nismeinert.de>
2017-10-25 21:33:22 +0200

    More sophisticated return values
    Now returns more 'reasonable' results for 'urls' 'players', 'player/0'
    and 'player/1'. Fall back: Empty string and status code 404.

--- server/src/webclient.cpp
+++ server/src/webclient.cpp
@@ -6,8 +6,19 @@ namespace utils  {
     }
 
 
-    std::string get(const std::string& /*url*/, int& statusCode) {
-        statusCode = 200;
-        return "{ key: 42 }";
+    std::string get(const std::string& url, int& statusCode) {
+        if (url == "players") {
+            statusCode = 200;
+            return "{ IDs: [0, 1] }";
+        } else if (url == "player/0") {
+            statusCode = 200;
+            return "{ key: \"000102030405060708090a0b0c0d0e0f\" }";
+        } else if (url == "player/1") {
+            statusCode = 200;
+            return "{ key: \"010102030405060708090a0b0c0d0e0f\" }";
+        } else {
+            statusCode = 404;
+            return "";
+        }
     }
 }
