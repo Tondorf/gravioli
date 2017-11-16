97ef28d9772c4b4c98bf6e0f1d21aea2fb628c4a
Nis Meinert <nis.meinert@gmail.com>
2017-11-16 11:09:04 +0100

    Removed Greenwich as separate class

--- server/src/main.cxx
+++ server/src/main.cxx
@@ -1,59 +1,36 @@
 #include <boost/asio.hpp>
+#include <chrono>
+#include <csignal>
 #include <cstdlib>
+#include <ctime>
 #include <functional>
 #include <future>
+#include <iomanip>
 #include <memory>
 #include <new>
+#include <ostream>
 #include <string>
 #include <vector>
 #include "SimpleLogger/logger.hpp"
 #include "config.hpp"
-#include "greenwich.hpp"
 #include "serializableWorld.hpp"
 #include "server.hpp"
 #include "simpleMsgQueue.hpp"
 #include "userInputParser.hpp"
 
 
-void awaitShutdown(boost::asio::signal_set &signal,
-                   std::function<void(boost::system::error_code, int)> cb) {
-    signal.add(SIGINT);
-    signal.add(SIGTERM);
-#if defined SIGQUIT
-    signal.add(SIGQUIT);
-#endif
-    signal.async_wait(cb);
-}
+void configureLogger();
+void awaitShutdown(boost::asio::signal_set &,
+                   std::function<void(boost::system::error_code, int)>);
 
 
 int main(int argc, char const* argv[]) {
-    auto logLevel = []() {
-        if (BUILD_TYPE == BuildType::RELEASE) {
-            return Log::LogLevel::INFO;
-        }
-        return Log::LogLevel::DEBUG;
-    }();
-    Log::SimpleLogger::getInstance().setLogLevel(logLevel);
-
-    /*
-     * Greenwich is initialize on first call of Greenwich::getInstance().
-     * Ensure, that following call is this first call!
-     */
-    Greenwich::getInstance();
-
-    Log::SimpleLogger::getInstance().addTimestamp([]() {
-        return Greenwich::getInstance().getFormatedTime() + " ";
-    });
-
     Config config;
     if (!parseUserInput(argc, argv, config)) {
         return EXIT_FAILURE;
     }
 
-    Log::info("Log level: %s", []() {
-        auto logLevel = Log::SimpleLogger::getInstance().getLogLevel();
-        return Log::SimpleLogger::logLevelAsString(logLevel);
-    }().c_str());
+    configureLogger();
 
     Log::info("Version: %d.%d (%s)",
               VERSION_MAJOR, VERSION_MINOR, BUILD_TYPE_AS_STRING);
@@ -114,4 +91,43 @@ int main(int argc, char const* argv[]) {
     }
 
     return EXIT_SUCCESS;
+}
+
+
+void configureLogger() {
+    auto logLevel = []() {
+        if (BUILD_TYPE == BuildType::RELEASE) {
+            return Log::LogLevel::INFO;
+        }
+        return Log::LogLevel::DEBUG;
+    }();
+    Log::SimpleLogger::getInstance().setLogLevel(logLevel);
+
+    std::locale::global(std::locale(LOGDATE_LOCALE));
+    Log::SimpleLogger::getInstance().addTimestamp([]() {
+        auto now = std::chrono::system_clock::to_time_t(
+            std::chrono::system_clock::now()
+        );
+
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&now), LOGDATE_FORMAT);
+
+        return ss.str() + " ";
+    });
+
+    Log::info("Log level: %s", []() {
+        auto logLevel = Log::SimpleLogger::getInstance().getLogLevel();
+        return Log::SimpleLogger::logLevelAsString(logLevel);
+    }().c_str());
+}
+
+
+void awaitShutdown(boost::asio::signal_set &signal,
+                   std::function<void(boost::system::error_code, int)> cb) {
+    signal.add(SIGINT);
+    signal.add(SIGTERM);
+#if defined SIGQUIT
+    signal.add(SIGQUIT);
+#endif
+    signal.async_wait(cb);
 }
